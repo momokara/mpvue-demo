7aee6e31d5b5b374c5600a16cbf9eaf2516c3908
momokara <1125076@qq.com>
2018-12-28 11:43:26 +0800

    完成field组件demo

--- src/pages/test/componentsDemo/Field/index.vue
+++ src/pages/test/componentsDemo/Field/index.vue
@@ -1,89 +1,169 @@
 <template>
   <div class="container">
-    <div id="msg">这里放页面内容</div>
-    <input type="text" v-model="msg">
-    <card :text="msg"></card>
+    <div class="title ">
+      Field 输入框
+    </div>
+    <div class="demo fsp14">
+      <div class="fsp16 fc-grey ">
+        基础用法
+      </div>
+      <van-cell-group>
+        <van-field
+          :value="value"
+          placeholder="请输入用户名"
+          :border="false"
+          clearable
+        />
+      </van-cell-group>
+
+      <div class="fsp16 fc-grey ">
+        自定义类型
+      </div>
+      <van-cell-group>
+        <van-field
+          :value="username "
+          label="用户名"
+          placeholder="请输入用户名"
+          clearable
+          icon="question-o"
+          icon-class="icon"
+          required
+          @click-icon="onClickIcon"
+        />
+        <van-field
+          :value="password"
+          type="password"
+          label="密码"
+          placeholder="请输入密码"
+          required
+          :border="false"
+        />
+      </van-cell-group>
+
+      <div class="fsp16 fc-grey ">
+        禁用输入框
+      </div>
+      <van-cell-group>
+        <van-field
+          value="输入框已禁用"
+          label="用户名"
+          left-icon="contact"
+          disabled
+          :border="false"
+        />
+      </van-cell-group>
+
+      <div class="fsp16 fc-grey ">
+        错误提示
+      </div>
+      <van-cell-group>
+        <van-field
+          :value="username2"
+          label="用户名"
+          placeholder="请输入用户名"
+          error
+        />
+        <van-field
+          :value="phone"
+          label="手机号"
+          placeholder="请输入手机号"
+          error-message="手机号格式错误"
+          :border="false"
+        />
+      </van-cell-group>
+
+      <div class="fsp16 fc-grey ">
+        高度自适应
+      </div>
+      <van-cell-group>
+        <van-field
+          :value="message"
+          label="留言"
+          type="textarea"
+          placeholder="请输入留言"
+          rows="1"
+          autosize
+          :border="false"
+        />
+      </van-cell-group>
+
+      <div class="fsp16 fc-grey ">
+        高度自适应
+      </div>
+      <van-cell-group>
+        <van-field
+          :value="sms"
+          center
+          clearable
+          label="短信验证码"
+          placeholder="请输入短信验证码"
+          use-button-slot
+          :border="false"
+        >
+          <van-button
+            slot="button"
+            size="small"
+            type="primary"
+            custom-class="button"
+          >发送验证码</van-button>
+        </van-field>
+      </van-cell-group>
+    </div>
+
+    <div class="info">
+      <div class="fsp16 fc-grey">API 说明</div>
+      <div class="fsp14 fc-grey">Field API</div>
+      <apiTable :tabledata="table_api_Field"></apiTable>
+
+      <div class="fsp14 fc-grey">Field Event</div>
+      <apiTable :tabledata="table_event_Field"></apiTable>
+
+      <div class="fsp14 fc-grey">Slot</div>
+      <apiTable :tabledata="table_Slot_Field"></apiTable>
+
+      <div class="fsp14 fc-grey">Slot</div>
+      <apiTable :tabledata="table_class_Field"></apiTable>
+
+    </div>
   </div>
 </template>
 <script>
-import card from "@/components/card";
+import apiTable from "@/components/api_data_table";
+import apiData from "./api_data";
 export default {
   data() {
     return {
-      msg: "这里是消息"
+      sms: "",
+      value: "",
+      password: "",
+      username: "",
+      username2: "",
+      message: "",
+      phone: "1365577",
+      table_api_Field:apiData.api_Field,
+      table_event_Field:apiData.event_Field,
+      table_event_Field:apiData.event_Field,
+      table_Slot_Field:apiData.slot_Field,
+      table_class_Field:apiData.class_Field,
     };
   },
   // 使用的 vue 组件
   components: {
-    card
+    apiTable
   },
   // 页面中的方法
-  methods: {},
-  // VUE 钩子 常用
-  // 页面初始化钩子 可以加载本地配置
-  beforeCreate() {
-    console.log("demopage-beforeCreate", this.msg);
+  methods: {
+    onClickIcon() {
+      wx.showToast({
+        icon: "none",
+        title: "点击图标"
+      });
+    }
   },
+
   // 页面创建时使用的钩子 可以开始处理页面中的异步请求数据
   created() {
     console.log("demopage-created", this.msg);
-  },
-  // 页面节点挂载之前 对请求数据进行处理
-  beforeMount() {
-    console.log("demopage-beforeMount", this.msg);
-  },
-  // 页面节点挂载  
-  mounted() {
-    console.log("demopage-mounted", this.msg);
-  },
-  // 页面跟新 之前 数据变更预处理
-  beforeUpdate() {
-    console.log("demopage-beforeUpdate", this.msg);
-  },
-  // 页面数据更新之后  数据变更处理
-  updated() {
-    console.log("demopage-updated", this.msg);
-  },
-  // 原生钩子
-  // 监听页面加载
-  onLoad() {
-    console.log("demopage-onLoad", this.msg);
-  },
-  // 监听页面显示
-  onShow() {
-    console.log("demopage-onShow", this.msg);
-  },
-  // 监听页面初次渲染完成
-  onReady() {
-    console.log("demopage-onReady", this.msg);
-  },
-  // 监听页面隐藏
-  onHide() {
-    console.log("demopage-onHide", this.msg);
-  },
-  // 监听页面卸载
-  onUnload() {
-    console.log("demopage-onUnload", this.msg);
-  },
-  // 监听用户下拉动作
-  onPullDownRefresh() {
-    console.log("demopage-onPullDownRefresh", this.msg);
-  },
-  // 页面上拉触底事件的处理函数
-  onReachBottom() {
-    console.log("demopage-onReachBottom", this.msg);
-  },
-  // 用户点击右上角分享
-  onShareAppMessage() {
-    console.log("demopage-onShareAppMessage", this.msg);
-  },
-  // 页面滚动
-  onPageScroll() {
-    console.log("demopage-onPageScroll", this.msg);
-  },
-  // 当前是 tab 页时，点击 tab 时触发 （mpvue 0.0.16 支持）
-  onTabItemTap() {
-    console.log("demopage-onTabItemTap", this.msg);
   }
 };
 </script>
